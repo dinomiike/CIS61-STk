cd3d7e3a6a303f17ced10f0680b855b3045c582c
Mike Ramos <Mike@Mike-Ramos-Computer.local>
2011-02-14 23:57:43 -0800

    Modified my answer to problem 4 and completed problem 6

--- Chapter1/assign2/p6.scm
+++ Chapter1/assign2/p6.scm
@@ -1,5 +1,10 @@
-(define (inc arg)
-  (+ arg 1))
+;; CIS61 Assignment 2
+;; Problem 6 (Exercise 1.41)
+;; double is a procedure that accepts a procedure as an argument and runs that argument twice on
+;; whatever parameter it gets. You need lambda to hold the parameter passed to the compound procedure
+;; as double itself just returns a procedure, not a value.
+;; The answer to the question in the book is 21, as seen in the attached .out file.
 
-(define (double fx)
-  (fx (double )
+(define (double f)
+  (lambda (x) (f (f x))))
+
